6229a8dec90920fc46ae6b713b0b2f999b18484f
Willian Murayama <willianyos13@gmail.com>
2023-03-29 11:01:11 -0300

    Fix main

--- Aulas/2023-03-15-Aula02/src/Classes/Main.java
+++ Aulas/2023-03-15-Aula02/src/Classes/Main.java
@@ -1,26 +1,11 @@
 package Classes;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-/**
- *
- * @author Willian Murayama
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Matriz matriz = new Matriz("src/Assets/lena256irfam.pgm");
-        Matriz skull = new Matriz("src/Assets/ctskull-256.pgm");
+
+        Imagem matriz = new Imagem("src/Assets/lena256irfam.pgm");
+        Imagem skull = new Imagem("src/Assets/ctskull-256.pgm");
 //        matriz.ImprimeArquivo("src/Assets/teste.pgm");
 //        matriz.ClarearAdicao(50).ImprimeArquivo("src/Assets/TesteClareadoAdicao.pgm");
 //        matriz.ClarearMultiplicao(2).ImprimeArquivo("src/Assets/TesteClareadoMultiplicacao.pgm");
