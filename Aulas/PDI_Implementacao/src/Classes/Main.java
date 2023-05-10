0dd04db15d340097fb8ff81bfbef77d2a24c7b2a
Willian Murayama <willianyos13@gmail.com>
2023-05-10 15:13:06 -0300

    Update Main.java

--- Aulas/PDI_Implementacao/src/Classes/Main.java
+++ Aulas/PDI_Implementacao/src/Classes/Main.java
@@ -16,8 +16,9 @@ public class Main {
         PPMImage lena = new PPMImage("src\\Assets\\ppm\\lenna.ppm");
         lena.saveImage("src\\Assets\\ppm\\lenappm.ppm");
         lena.saveImageChannels("src\\Assets\\ppm\\lenappm");
-        lena.Clarear(0, 0, 100).saveImage("src\\Assets\\ppm\\clarearA.ppm");
+        lena.Clarear(0, 0, 100).saveImage("src\\Assets\\ppm\\clarearB.ppm");
         lena.Escurecer(100, 0, 0).saveImage("src\\Assets\\ppm\\escurecerR.ppm");
+        lena.InvertChannels("b", "g", "r").saveImage("src\\Assets\\ppm\\Lena.ppm");
     }
 
 }
