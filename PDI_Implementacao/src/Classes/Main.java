92fe95a591e321647e2e072add23a129d7207904
Willian Murayama <willianyos13@gmail.com>
2023-05-16 11:29:46 -0300

    Update Main.java

--- PDI_Implementacao/src/Classes/Main.java
+++ PDI_Implementacao/src/Classes/Main.java
@@ -21,11 +21,11 @@ public class Main {
 //        lena.Escurecer(100, 0, 0).saveImage("src\\Assets\\ppm\\escurecerR.ppm");
         PGMImage lenaAscii = new PGMImage("src\\Assets\\originais\\lena256.pgm");
 
-        PGMImage lenaBinary = new PGMImage("src\\Assets\\originais\\lenabinary.pgm");
+        PPMImage lenaBinary = new PPMImage("src\\Assets\\originais\\lennabinary.ppm");
         lenaBinary.saveImage("src\\Assets\\todos\\lenabinarypgm.pgm");
         lenaAscii.saveImage("src\\Assets\\todos\\lenaascii.pgm");
 
-        //        lena.InvertChannels("r", "g", "b").saveImage("src\\Assets\\ppm\\RGB.ppm");
+        lenaBinary.InvertChannels("r", "g", "b").saveImage("src\\Assets\\ppm\\RGB.ppm");
         //        lena.InvertChannels("r", "b", "g").saveImage("src\\Assets\\ppm\\RBG.ppm");
         //        lena.InvertChannels("g", "r", "b").saveImage("src\\Assets\\ppm\\GRB.ppm");
         //        lena.InvertChannels("g", "b", "r").saveImage("src\\Assets\\ppm\\GBR.ppm");
