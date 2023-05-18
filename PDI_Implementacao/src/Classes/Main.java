5025a632eb56cf465f6aa76739a2a8415db0ef5e
Willian Murayama <willianyos13@gmail.com>
2023-05-18 16:29:32 -0300

    Update Main.java

--- PDI_Implementacao/src/Classes/Main.java
+++ PDI_Implementacao/src/Classes/Main.java
@@ -20,17 +20,19 @@ public class Main {
 //        lena.Clarear(0, 0, 100).saveImage("src\\Assets\\ppm\\clarearB.ppm");
 //        lena.Escurecer(100, 0, 0).saveImage("src\\Assets\\ppm\\escurecerR.ppm");
         PGMImage lenaAscii = new PGMImage("src\\Assets\\originais\\lena256.pgm");
+        PGMImage quadrados = new PGMImage("src\\Assets\\originais\\quadrados.pgm");
+        //PPMImage lenaBinary = new PPMImage("src\\Assets\\originais\\lennabinary.ppm");
+        //lenaBinary.saveImage("src\\Assets\\todos\\lenabinarypgm.pgm");
+        //lenaAscii.saveImage("src\\Assets\\todos\\lenaascii.pgm");
 
-        PPMImage lenaBinary = new PPMImage("src\\Assets\\originais\\lennabinary.ppm");
-        lenaBinary.saveImage("src\\Assets\\todos\\lenabinarypgm.pgm");
-        lenaAscii.saveImage("src\\Assets\\todos\\lenaascii.pgm");
-
-        lenaBinary.InvertChannels("r", "g", "b").saveImage("src\\Assets\\ppm\\RGB.ppm");
+        //lenaBinary.InvertChannels("r", "g", "b").saveImage("src\\Assets\\ppm\\RGB.ppm");
         //        lena.InvertChannels("r", "b", "g").saveImage("src\\Assets\\ppm\\RBG.ppm");
         //        lena.InvertChannels("g", "r", "b").saveImage("src\\Assets\\ppm\\GRB.ppm");
         //        lena.InvertChannels("g", "b", "r").saveImage("src\\Assets\\ppm\\GBR.ppm");
         //        lena.InvertChannels("b", "r", "g").saveImage("src\\Assets\\ppm\\BRG.ppm");
         //        lena.InvertChannels("b", "g", "r").saveImage("src\\Assets\\ppm\\BGR.ppm");
+        quadrados.equalizacaoLocalHistograma(3).saveImage("src\\Assets\\todos\\quadrados.pgm");
+        lenaAscii.media(3).saveImage("src\\Assets\\todos\\lenamedia.pgm");
     }
 
 }
