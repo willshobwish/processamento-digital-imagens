3e809fa3c476703cf8547d0cfd996cfbfc1718d4
Willian Murayama <willianyos13@gmail.com>
2023-05-18 16:28:35 -0300

    Removed import

--- PDI_Implementacao/src/Classes/PGMImage.java
+++ PDI_Implementacao/src/Classes/PGMImage.java
@@ -1,7 +1,5 @@
 package Classes;
 
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
