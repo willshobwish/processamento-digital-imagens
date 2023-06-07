8792874e249cf9b5a43ad0a5e199572812d29f3f
Willian Murayama <willianyos13@gmail.com>
2023-06-06 22:12:06 -0300

    Criação do projeto inicial

---
+++ Trabalho/src/main/java/com/mycompany/trabalho/Trabalho.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalho;
+
+/**
+ *
+ * @author willm
+ */
+public class Trabalho {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
