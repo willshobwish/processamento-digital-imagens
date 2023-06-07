e297d75563a51f6fe474e2cdda2dc9ae7778a9f7
Willian Murayama <willianyos13@gmail.com>
2023-06-07 12:41:32 -0300

    Instanciação do controlador pela main

---
+++ Trabalho/src/main/java/controller/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+/**
+ *
+ * @author Willian
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Controlador.getInstance();
+    }
+}
